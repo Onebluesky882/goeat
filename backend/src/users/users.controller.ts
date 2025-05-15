03c4cb9642e760f9f86db3278ceb2c263378aa2d
Onebluesky882 <onebluesky882@outlook.com>
2025-05-15 17:12:16 +0700

    authorization

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './user.dto';
 import { AuthGuard } from '@nestjs/passport';
+
 @UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
