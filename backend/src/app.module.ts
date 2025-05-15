f17743ee89412f08b6bb25e6f44ab79521d7d976
Onebluesky882 <onebluesky882@outlook.com>
2025-05-15 21:40:19 +0700

    migration db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TodoModule } from './todos/todo.module';
 import { UsersService } from './users/users.service';
 import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
@@ -17,7 +16,6 @@ import { AppController } from './app.controller';
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DatabaseModule,
-    TodoModule,
     UsersModule,
     AuthModule,
   ],
