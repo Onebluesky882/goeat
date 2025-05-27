d76218725e3e8b8f2840e76413a7677611580330
Onebluesky882 <onebluesky882@outlook.com>
2025-05-27 21:20:29 +0700

    clear import

--- frontend/src/components/shops/ShopBars.tsx
+++ frontend/src/components/shops/ShopBars.tsx
@@ -1,7 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { LuSquareMenu } from "react-icons/lu";
 import Clock from "../Clock";
-import { useShopStore } from "@/stores/shopStore";
 type ShopProps = {
   id: string;
   name: string;
